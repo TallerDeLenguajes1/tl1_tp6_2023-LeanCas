378637c20859ac119524f5aa23721e0a2b9cf5b6
Leandro Casavalle <leandrocasavalle10@gmail.com>
2023-05-19 14:48:32 -0300

    Ejercicio 1

---
+++ practico6/Program.cs
@@ -0,0 +1,40 @@
+﻿
+bool isInteger;
+
+do
+{
+    Console.WriteLine("Ingrese un numero entero : ");
+
+    string input = Console.ReadLine();
+
+    int number;
+
+    isInteger = int.TryParse(input, out number); // isInteger is a  bool who confirmed if input is a word or not, if is a number, the value of input is save in the variable number
+
+    if (isInteger == true)
+    {
+
+        while (number > 9)
+        {
+
+            Console.Write(number % 10);
+
+            number = number / 10;
+
+
+        }
+
+        Console.Write(number);
+
+    }
+    else
+    {
+
+        Console.WriteLine("No ingreso un numero, intente nuevamente...");
+
+    }
+
+} while (isInteger == false);
+
+
+
